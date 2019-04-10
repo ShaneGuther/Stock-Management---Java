0c227321ade9f9689e5a9b28bfb2c5907949744a
shaneguther <shaneguther@gmail.com>
2019-04-09 21:22:09 -0400

    updated file reading to fix error added itemID added button functionality in the additional screens and table updates

--- src/finalproject/UpdateWindowController.java
+++ src/finalproject/UpdateWindowController.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -51,10 +52,14 @@ public class UpdateWindowController implements Initializable {
 
     @FXML
     private void acptBtnHandler(ActionEvent event) {
+        Stage stage = (Stage) cancelBtn.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
     private void cancelBtnHandler(ActionEvent event) {
+        Stage stage = (Stage) cancelBtn.getScene().getWindow();
+        stage.close();
     }
     
     @FXML
