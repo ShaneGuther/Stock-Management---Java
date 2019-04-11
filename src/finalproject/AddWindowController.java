85ba33b0599565071642f0a49b110010433ec607
shaneguther <shaneguther@gmail.com>
2019-04-10 23:10:33 -0400

    Merge shaneTwo
    Conflicts:
    	src/finalproject/AddWindowController.java
    	src/finalproject/DeleteWindow.fxml
    	src/finalproject/DeleteWindowController.java
    	src/finalproject/FXMLDocument.fxml
    	src/finalproject/FXMLDocumentController.java
    	src/finalproject/FileManagement.java

--- src/finalproject/AddWindowController.java
+++ src/finalproject/AddWindowController.java
@@ -13,7 +13,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
